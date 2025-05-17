02cbd6e4192de9c732a5b8180eccd7e05a115709
bizzy-coding <bizzycoding@gmail.com>
2025-05-17 21:11:13 +0100

    adds night and day use state selection

--- src/components/useState/Counter2.jsx
+++ src/components/useState/Counter2.jsx
@@ -4,9 +4,12 @@ export default function Counter2() {
 
     const [counter, setCounter] = useState(0)
     const [name, setName] = useState("")
+    const [isDay, setIsDay] = useState(false);
 
     return (
         <>
+
+        <div className="wrapper">
 
       <h1>Hi {name ? name : "newbie"}</h1>
       <input 
@@ -16,12 +19,34 @@ export default function Counter2() {
         onChange={(e) => setName(e.target.value)}
         />
         <label htmlFor="input"> ⬅️ enter your name to personalise experience</label>
+        </div>
 
+        <div className="wrapper">
         <p>You clicked {counter} times</p>
 
         <button onClick={() => setCounter(counter + 1)}>Click me to add one</button>
 
-    
+        </div>
+
+        <div className="wrapper">
+
+        <button onClick={() => setIsDay(!isDay)}>
+  {isDay ? "Switch to night" : "Switch to day"}
+</button>
+
+<p>It is {isDay ? "🌞" : "🌚"} time</p>
+
+
+
+
+
+            </div>
+
+            <div className="wrapper">
+            </div>
+
+            <div className="wrapper">
+            </div>
        
         </>
     )
